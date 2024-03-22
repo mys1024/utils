15fab2c55bfc02de88e61871019ae4725b8ab489
mys1024 <mys.1024@outlook.com>
2024-03-23 04:15:44 +0800

    test: update tests

--- src/assert.ts
+++ src/assert.ts
@@ -4,39 +4,47 @@
 
 import { AssertionError } from "@std/assert";
 
-export async function assertDurationMeets(
+export function assertDurationMeets(
   promise: Promise<any>,
   options: {
     min?: number;
     max?: number;
   } = {},
 ) {
-  const { min = -1, max = -1 } = options;
-  let timeoutId: number | undefined;
-  if (max !== -1) {
-    timeoutId = setTimeout(() => {
-      throw new AssertionError(
-        "The promise is taking too long to resolve or reject.",
-      );
-    }, max + 100);
-  }
-  const start = Date.now();
-  try {
-    await promise;
-  } catch {
-    // ignore
-  }
-  clearTimeout(timeoutId);
-  const end = Date.now();
-  const duration = end - start;
-  if (min !== -1 && duration < min) {
-    throw new AssertionError(
-      `The promise took ${duration}ms to resolve or reject, which is less than the minimum duration of ${min}ms.`,
-    );
-  }
-  if (max !== -1 && duration > max) {
-    throw new AssertionError(
-      `The promise took ${duration}ms to resolve or reject, which is more than the maximum duration of ${max}ms.`,
-    );
-  }
+  return new Promise<void>((resolve, reject) => {
+    const { min = -1, max = -1 } = options;
+    let timeoutId: number | undefined;
+    if (max !== -1) {
+      timeoutId = setTimeout(() => {
+        reject(
+          new AssertionError(
+            "The promise is taking too long to resolve or reject.",
+          ),
+        );
+      }, max + 100);
+    }
+    const start = Date.now();
+    promise
+      .catch(() => {})
+      .finally(() => {
+        const end = Date.now();
+        clearTimeout(timeoutId);
+        const duration = end - start;
+        if (min !== -1 && duration < min) {
+          reject(
+            new AssertionError(
+              `The promise took ${duration}ms to resolve or reject, which is less than the minimum duration of ${min}ms.`,
+            ),
+          );
+        }
+        if (max !== -1 && duration > max) {
+          reject(
+            new AssertionError(
+              `The promise took ${duration}ms to resolve or reject, which is more than the maximum duration of ${max}ms.`,
+            ),
+          );
+        }
+        resolve();
+      });
+  });
 }
--- test/assert.test.ts
+++ test/assert.test.ts
@@ -2,19 +2,50 @@ import { AssertionError, assertRejects } from "@std/assert";
 import { delay } from "../src/main.ts";
 import { assertDurationMeets } from "../src/assert.ts";
 
-Deno.test("assertDurationMeets()", async () => {
-  await Promise.all([
-    assertDurationMeets(delay(100), { min: 100, max: 120 }),
-    assertDurationMeets(delay(200), { min: 200, max: 220 }),
-  ]);
-  await assertRejects(
-    () => assertDurationMeets(delay(100), { min: 200, max: 210 }),
-    AssertionError,
-    "which is less than the minimum duration of 200ms",
-  );
-  await assertRejects(
-    () => assertDurationMeets(delay(200), { min: 100, max: 110 }),
-    AssertionError,
-    "which is more than the maximum duration of 110ms",
-  );
+Deno.test("assertDurationMeets()", async (t) => {
+  await t.step("meets", async () => {
+    await Promise.all([
+      assertDurationMeets(delay(100), { min: 100, max: 120 }),
+      assertDurationMeets(delay(200), { min: 200, max: 220 }),
+    ]);
+  });
+
+  await t.step("min & max", async () => {
+    await assertRejects(
+      () => assertDurationMeets(delay(100), { min: 200, max: 210 }),
+      AssertionError,
+      "which is less than the minimum duration of 200ms",
+    );
+    await assertRejects(
+      () => assertDurationMeets(delay(200), { min: 100, max: 110 }),
+      AssertionError,
+      "which is more than the maximum duration of 110ms",
+    );
+  });
+
+  await t.step("timeout", async () => {
+    let timeoutId: number | undefined;
+    const promise = new Promise<void>((resolve) => {
+      timeoutId = setTimeout(resolve, 2000);
+    });
+    await assertRejects(
+      () => assertDurationMeets(promise, { min: 100, max: 110 }),
+      AssertionError,
+      "The promise is taking too long to resolve or reject.",
+    );
+    clearTimeout(timeoutId);
+  });
+
+  await t.step("promise rejects", async () => {
+    let timeoutId: number | undefined;
+    const promise = new Promise<void>((_resolve, reject) => {
+      timeoutId = setTimeout(() => reject("Reject!"), 120);
+    });
+    await assertRejects(
+      () => assertDurationMeets(promise, { min: 100, max: 110 }),
+      AssertionError,
+      "which is more than the maximum duration of 110ms",
+    );
+    clearTimeout(timeoutId);
+  });
 });
